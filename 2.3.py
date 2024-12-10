daed8f487e4496716fffee7434f1f3acb358d569
phamthehop <phamhuyennghi2016@gmail.com>
2024-12-10 07:58:14 +0700

    Create 2.3.py

---
+++ 2.3.py
@@ -0,0 +1,16 @@
+import math
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+delta = b**2 - 4*a*c
+if delta > 0:
+    x1 = (-b + math.sqrt(delta)) / (2 * a)
+    x2 = (-b - math.sqrt(delta)) / (2 * a)
+    print(f"x1 = {x1:.2f}  x2 = {x2:.2f}")
+elif delta == 0:
+    x = -b / (2 * a)
+    print(f"x = {x:.2f}")
+else:
+    print("VN")
