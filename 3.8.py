4183d4d3772a5f4278791e158e396aa9143cc0e3
phamthehop <phamhuyennghi2016@gmail.com>
2024-12-10 09:31:53 +0700

    Create 3.8.py

---
+++ 3.8.py
@@ -0,0 +1,12 @@
+def muoisanghai(n):
+    if n == 0:  
+        return "0"
+    nhiphan = ""
+    while n > 0:
+        nhiphan=str(n % 2) + nhiphan
+        n // = 2
+    return nhiphan
+
+n = int(input())
+kq = muoisanghai(n)
+print(kq)
