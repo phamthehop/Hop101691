c67298684c0e2442452022b349b915af7541fbf9
phamthehop <phamhuyennghi2016@gmail.com>
2024-12-10 08:32:39 +0700

    Create 2.13.py

---
+++ 2.13.py
@@ -0,0 +1,15 @@
+import math
+
+n = int(input())
+S = 0
+for i in range(1, 2*n + 1,2):
+    S += 1/math.factorial(i)
+print(S)
+
+m = int(input())
+T=0
+for i in range(1,m+1):
+    T=T+i
+    if(T>1000):
+        print(f"Số thoả mãn: {i}")
+        break
