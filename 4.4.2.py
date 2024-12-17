7c57944ad9f117ee3523f083723fd0c386cab2e6
phamthehop <phamhuyennghi2016@gmail.com>
2024-12-17 08:08:10 +0700

    Create 4.4.2.py

---
+++ 4.4.2.py
@@ -0,0 +1,28 @@
+class Book:
+    def __init__(self):
+        self.name = ""
+        self.page = 0
+        self.cost = 0
+
+    def nhap(self):
+        self.name = input()
+        self.page = int(input())
+        self.cost = int(input())
+        self.average = round(self.page/self.cost,2)
+
+    def xuat(self):
+        print("{0} {1} {2} {3}".format(self.name,self.page,self.cost,self.average))
+        
+print("Nhập số lượng sách: ")
+n = int(input())
+books = []
+for i in range(n):
+    print("Nhap quyen sach thu",i+1)
+    book = Book()
+    book.nhap()
+    books.append(book)
+
+books.sort(key=lambda x:(x.average,x.name))
+
+for book in books:
+    book.xuat()
