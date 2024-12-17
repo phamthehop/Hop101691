e1be740cb50c32eb1d5c645297a22e72794ffee4
phamthehop <phamhuyennghi2016@gmail.com>
2024-12-17 08:34:40 +0700

    Update 4.4.2.py

--- 4.4.2.py
+++ 4.4.2.py
@@ -12,6 +12,9 @@ class Book:
 
     def xuat(self):
         print("{0} {1} {2} {3}".format(self.name,self.page,self.cost,self.average))
+    
+    def ghi_vao_file(self, f):
+        f.write("{0} {1} {2} {3}\n".format(self.name, self.page, self.cost, self.average))
         
 print("Nhập số lượng sách: ")
 n = int(input())
@@ -26,3 +29,7 @@ books.sort(key=lambda x:(x.average,x.name))
 
 for book in books:
     book.xuat()
+    
+with open("sach.txt", "w", encoding="utf-8") as f:
+    for book in books:
+        book.ghi_vao_file(f)
