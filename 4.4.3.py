af6fa8ac136f01a3662385a294d0e9c870559524
phamthehop <phamhuyennghi2016@gmail.com>
2024-12-17 08:42:21 +0700

    Update 4.4.3.py

--- 4.4.3.py
+++ 4.4.3.py
@@ -7,20 +7,21 @@ class Book:
     def __str__(self):
         return f"{self.name} {self.page} {self.cost}"
 
+    def ghi_vao_file(self, f):
+        f.write(f"{self.name} {self.page} {self.cost}\n")
+
 books = []
-
 with open("sach.txt", "r", encoding="utf-8") as f:
-    for line in f:
-        data = line.strip().split()
-        name = " ".join(data[:-3]) 
-        page = int(data[-2])     
-        cost = int(data[-1])     
+    lines = f.readlines()  
+    for i in range(0, len(lines), 3):
+        name = lines[i].strip()     
+        page = int(lines[i + 1].strip()) 
+        cost = int(lines[i + 2].strip()) 
         books.append(Book(name, page, cost))
-
 
 loc_books = [book for book in books if book.cost > 100000 and book.page < 200]
 
-
 with open("ketqua.txt", "w", encoding="utf-8") as f:
+    f.write("Danh sách các sách lọc theo điều kiện:\n") 
     for book in loc_books:
-        f.write(str(book) + "\n")
+        book.ghi_vao_file(f)
