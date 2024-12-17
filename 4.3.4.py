af469a0a01690a8f138512e0ef65a1304b5ee1f2
phamthehop <phamhuyennghi2016@gmail.com>
2024-12-17 07:57:27 +0700

    Create 4.3.4.py

---
+++ 4.3.4.py
@@ -0,0 +1,18 @@
+m,n = map(int,input().split())
+A = []
+for _ in range(m):
+    A.append(list(map(int,input().split())))
+B = []
+for _ in range(m):
+    B.append(list(map(int,input().split())))
+
+C = []
+for i in range(m):
+    dongC = []
+    for j in range(n):
+        dongC.append(A[i][j] +B[i][j])
+    C.append(dongC)
+
+for dong in C:
+    print(*dong)
+        
