a1c378c08468b002304653aea35fad5dc6aa17d6
phamthehop <phamhuyennghi2016@gmail.com>
2024-12-17 08:12:23 +0700

    Create 4.4.5.py

---
+++ 4.4.5.py
@@ -0,0 +1,27 @@
+class NV:
+    def __init__(self):
+        pass
+
+    def nhap(self):
+        
+        self.ten,self.ma,self.hesoluong,self.phucap = input().split()
+        self.ma = int(self.ma)
+        self.hesoluong = float(self.hesoluong)
+        self.phucap = int(self.phucap)
+        self.luong = self.hesoluong*2000000+self.phucap
+    
+    def xuat(self):
+        print("{0} {1} {2:.2f} {3} {4:.2f}".format(self.ma,self.ten,self.hesoluong,self.phucap,self.luong))
+
+n = int(input())
+dsnv = []
+for i in range(n):
+    nv=NV()
+    nv.nhap()
+    dsnv.append(nv)
+    
+dsnv.sort(reverse=True,key=lambda x:x.luong)
+
+print("Danh sach nhan vien da sap xep:",n)
+for i in dsnv:
+    i.xuat()
