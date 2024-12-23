f1f2e210be4943ade8d85f70b4eeeeef318df214
phamthehop <phamhuyennghi2016@gmail.com>
2024-12-23 14:28:59 +0700

    Update 2.16.py

--- 2.16.py
+++ 2.16.py
@@ -1,13 +1,13 @@
 import math
 
 a,b,c = map(int,input().split())
-if a + b > c and a + c > b and b + c >a:
+if a + b > c and a + c > b and b + c > a:
     print("YES")
 else:
     print("NO")
     
 chuvi = a + b + c
-s = (a + b +c)/2
+s = (a + b +c) / 2
 dientich = math.sqrt(s * (s - a) * (s - b) * (s - c))
 print(chuvi)
 print(dientich)
