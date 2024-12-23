425246b0b56f33fe66760118733b82f0db44522c
phamthehop <phamhuyennghi2016@gmail.com>
2024-12-23 16:26:27 +0700

    Update 4.3.4.py

--- 4.3.4.py
+++ 4.3.4.py
@@ -10,7 +10,7 @@ C = []
 for i in range(m):
     dongC = []
     for j in range(n):
-        dongC.append(A[i][j] +B[i][j])
+        dongC.append(A[i][j] + B[i][j])
     C.append(dongC)
 
 for dong in C:
